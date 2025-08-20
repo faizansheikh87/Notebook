06b1a585cfddefec8ece8094a211b2ef71d1225b
Faizan Sheikh <149717142+faizansheikh87@users.noreply.github.com>
2025-08-20 19:38:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 const PORT = Process.env.PORT || 3001;
 const app = express();
-const Sceret_Key = "FaizanIsGreat"
+
 
 // Middleware to parse JSON body
 app.use(express.json());
@@ -127,7 +127,7 @@ app.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Incorrect password" });
     }
      
-    const token = jwt.sign({id:user._id},Sceret_Key,{expiresIn:'1h'});
+    const token = jwt.sign({id:user._id},Process.env.JWT_SECRET,{expiresIn:'1h'});
     res.status(200).json({token:token,email:user.email});
   } catch (err) {
     console.log("Login error:", err);
